eb365a374101ede90025f12ea13eee0f6a1c73ec
maryamdkh <m.dadkhah99@gmail.com>
2025-03-01 14:15:40 +0330

    change config file

---
+++ test/test.py
@@ -0,0 +1,10 @@
+
+# import sys
+# sys.path.append("C:\\Users\\maryam\\Documents\\Columbia")
+# print(sys.path)
+
+# from SpeechCARE_Acoustic_Explainability_Framework.models.Model import TBNet
+# from SpeechCARE_Acoustic_Explainability_Framework.Config import Config
+
+# config = Config()
+# print(config.txt_transformer_chp)
